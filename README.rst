47c2a27dffcd9a6bb4e28d5b5f86eb6cca00a17c
dong wang <wangd40@chinatelecom.cn>
2023-06-15 16:47:36 +0800

    Wrapping C/C++ for Python

---
+++ README.rst
@@ -0,0 +1,4 @@
+pyAiDB: Wrapping AiDB for Python.
+=========================================
+:Gitlab: https://github.com/TalkUHulk/ai.deploy.box
+:Talk is cheap,show me the code.
---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+import pathlib
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeExtension(Extension):
+
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+
+    def run(self):
+        for ext in self.extensions:
+            if isinstance(ext, CMakeExtension):
+                self.build_cmake(ext)
+        super().run()
+
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
+
+        build_temp = f"{pathlib.Path(self.build_temp)}/{ext.name}"
+        os.makedirs(build_temp, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        config = "Debug" if self.debug else "Release"
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + str(extdir.parent.absolute()),
+            "-DCMAKE_BUILD_TYPE=" + config, "-DBUILD_PYTHON=ON",
+            "-DBUILD_SAMPLE=OFF", "-DENGINE_NCNN_WASM=OFF",
+            "-DENGINE_MNN=ON", "-DENGINE_ORT=ON", "-DENGINE_NCNN=ON",
+            "-DENGINE_TNN=ON", "-DENGINE_OPV=ON", "-DENGINE_PPLite=ON"
+        ]
+
+        build_args = [
+            "--config", config,
+            "--", "-j8"
+        ]
+
+        os.chdir(build_temp)
+        self.spawn(["cmake", f"{str(cwd)}/{ext.name}"] + cmake_args)
+        if not self.dry_run:
+            self.spawn(["cmake", "--build", "."] + build_args)
+        os.chdir(str(cwd))
+
+
+setup(
+    ext_modules=[CMakeExtension('.')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    include_package_data=True
+)
