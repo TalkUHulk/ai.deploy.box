9850dae259bf244356f83f65cc78c50fae34bc85
dong wang <wangd40@chinatelecom.cn>
2023-08-18 17:07:10 +0800

    update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 #source set_env.sh
 
 CURRENT_DIR=$(dirname $(realpath ${BASH_SOURCE}))
-lib_path="${CURRENT_DIR}/libs/linux"
-lib_tnn_path="${CURRENT_DIR}/libs/linux/tnn"
-lib_openvino_path="${CURRENT_DIR}/libs/linux/openvino"
-lib_paddlelite_path="${CURRENT_DIR}/libs/linux/paddlelite"
+lib_path="${CURRENT_DIR}/libs/linux/x86_64"
+lib_tnn_path="${CURRENT_DIR}/libs/linux/x86_64/tnn"
+lib_openvino_path="${CURRENT_DIR}/libs/linux/x86_64/openvino"
+lib_paddlelite_path="${CURRENT_DIR}/libs/linux/x86_64/paddlelite"
 
 
 LD_LIBRARY_PATH=${lib_path}:${lib_tnn_path}:${lib_openvino_path}:${lib_paddlelite_path}:$LD_LIBRARY_PATH
